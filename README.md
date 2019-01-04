2ce9e7e62566bc917ea892a126b9f6a5fb743e3b
Chanda Grayson <chandagrayson@gmail.com>
2019-01-04 12:20:50 -0500

    crushed it

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ For this assignment, we'll be working with a Yelp-style domain. We have three mo
 For our purposes, a `Restaurant` has many `Reviews`, a `Customer` has many `Reviews`, and a `Review` belongs to a `Customer` and to a `Restaurant`.
 `Restaurant` - `Customer` is a many to many relationship.
 
+RESTAURANT < REVIEW > CUSTOMER
+
 If you are not sketching out your domain, and thinking about single source of truth,
 you are doing it wrong :(
 
@@ -29,8 +31,8 @@ We've provided you with a console that you can use to test your code. To enter a
 
 #### Build the following methods on the `Customer` class
 
-- `Customer.all`
-  - should return **all** of the customer instances
+<!-- - `Customer.all`
+  - should return **all** of the customer instances -->
 - `Customer.find_by_name(name)`
   - given a string of a **full name**, returns the **first customer** whose full name matches
 - `Customer.find_all_by_first_name(name)`
@@ -42,8 +44,8 @@ We've provided you with a console that you can use to test your code. To enter a
 
 #### Build out the following methods on the `Restaurant` class
 
-- `Restaurant.all`
-  - returns an array of all restaurants
+<!-- - `Restaurant.all`
+  - returns an array of all restaurants -->
 - `Restaurant.find_by_name(name)`
   - given a string of restaurant name, returns the first restaurant that matches
 
@@ -51,8 +53,8 @@ We've provided you with a console that you can use to test your code. To enter a
 
 #### Build out the following methods on the `Review` class
 
-- `Review.all`
-  - returns all of the reviews
+<!-- - `Review.all`
+  - returns all of the reviews -->
 - `Review#customer`
   - returns the customer object for that given review
   - Once a review is created, I should not be able to change the author
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,64 @@
 class Customer
-  attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
+  attr_accessor :first_name, :last_name, :full_name
+
+  @@all = []
+
+  def initialize(first_name, last_name, full_name)
     @first_name = first_name
     @last_name  = last_name
+    @full_name = full_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+
+  # - `Customer.all`
+  #   - should return **all** of the customer instances
+  def self.all
+    @@all
+  end
+
+  # - `Customer.find_by_name(name)`
+  #   - given a string of a **full name**, returns the **first customer** whose full name matches
+  def self.find_by_name(name)
+    @@all.find do |customer|
+      customer.name == full_name
+      #didnt have time to test, not sure this is correct, but I think it will need to reference back to the above full_name method
+    end
+  end
+
+  # - `Customer.find_all_by_first_name(name)`
+  #   - given a string of a first name, returns an **array** containing all customers with that first name
+  def self.find_all_by_first_name(name)
+    @@all.find do |customer|
+      customer.name == first_name
+      #no time to test, but this looks correct to me
+    end
+  end
+
+  # - `Customer.all_names`
+  #   - should return an **array** of all of the customer full names
+  def self.all_names(full_name)
+    all_names = []
+    all_names << @full_name
+    binding.pry
+    #created @full_name & updated console to reflect that additional string hoping to be able to shovel that into
+    #a new array... short on time, but now I'm wondering if I should've used a .join to bring the first & last names together... 
+  end
+
+
+
+  # - `Customer#add_review(restaurant, content, rating)`
+  #   - given a **restaurant object**, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+  # - `Customer#num_reviews`
+  #   - Returns the total number of reviews that a customer has authored
+  # - `Customer#restaurants`
+  #   - Returns a **unique** array of all restaurants a customer has reviewed
+
+
+
 end
